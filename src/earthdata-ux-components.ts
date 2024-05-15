6159d9611f71f9c2a38b730a6d6285ae4f29b5cf
aritrivi <anthony.j.ritrivi@nasa.gov>
2024-05-15 14:58:01 -0400

    Removing duplicate exports for button and icon.

--- src/earthdata-ux-components.ts
+++ src/earthdata-ux-components.ts
@@ -7,8 +7,6 @@ export { default as EduxPlot } from './components/plot/plot.js'
 export { default as EduxSpatialPicker } from './components/spatial-picker/spatial-picker.js'
 export { default as EduxTimeSeries } from './components/time-series/time-series.js'
 export { default as EduxVariableCombobox } from './components/variable-combobox/variable-combobox.js'
-export { default as EduxButton } from './components/button/button.js'
-export { default as EduxIcon } from './components/icon/icon.js'
 export { default as EduxLoader } from './components/loader/loader.js';
 
 /* plop:component */
