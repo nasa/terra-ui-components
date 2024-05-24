b21d8c92e801a5ad4c9d5417ac6a6669d0b19d10
aritrivi <anthony.j.ritrivi@nasa.gov>
2024-05-24 12:20:51 -0400

    Convert px to rem

--- src/styles/component.styles.ts
+++ src/styles/component.styles.ts
@@ -21,43 +21,43 @@ export default css`
 
     .display-120 {
         font-family: var(--edux-font-display);
-        font-size: 120px;
+        font-size: 7.5rem; /* 120px */
         font-weight: var(--edux-font-bold);
     }
 
     .display-100 {
         font-family: var(--edux-font-display);
-        font-size: 100px;
+        font-size: 6.25rem; /* 100px */
         font-weight: var(--edux-font-bold);
     }
 
     .display-80 {
         font-family: var(--edux-font-display);
-        font-size: 80px;
+        font-size: 5rem; /* 80px */
         font-weight: var(--edux-font-bold);
     }
 
     .display-72 {
         font-family: var(--edux-font-display);
-        font-size: 72px;
+        font-size: 4.5rem; /* 72px */
         font-weight: var(--edux-font-bold);
     }
 
      .display-60 {
         font-family: var(--edux-font-display);
-        font-size: 60px;
+        font-size: 3.75rem; /* 60px */
         font-weight: var(--edux-font-bold);
     }
     
     .display-48 {
         font-family: var(--edux-font-display);
-        font-size: 48px;
+        font-size: 3rem; /* 48px */
         font-weight: var(--edux-font-bold);
     }
 
     .display-41 {
         font-family: var(--edux-font-display);
-        font-size: 41px;
+        font-size: 2.563rem; /* 41px */
         font-weight: var(--edux-font-bold);
     }
 
@@ -65,97 +65,97 @@ export default css`
 
     .heading-36-bold {
         font-family: var(--edux-font-heading);
-        font-size: 36px;
+        font-size: 2.25rem; /* 36px */
         font-weight: var(--edux-font-bold);
     }
 
     .heading-36-light {
         font-family: var(--edux-font-heading);
-        font-size: 36px;
+        font-size: 2.25rem; /* 36px */
         font-weight: var(--edux-font-light);
     }
 
     .heading-29-bold {
         font-family: var(--edux-font-heading);
-        font-size: 29px;
+        font-size: 1.813rem; /* 29px */
         font-weight: var(--edux-font-bold);
     }
 
     .heading-29-light {
         font-family: var(--edux-font-heading);
-        font-size: 29px;
+        font-size: 1.813rem; /* 29px */
         font-weight: var(--edux-font-light);
     }
 
     .heading-22-bold {
         font-family: var(--edux-font-heading);
-        font-size: 22px;
+        font-size: 1.375rem; /* 22px */
         font-weight: var(--edux-font-bold);
     }
 
     .heading-22-light {
         font-family: var(--edux-font-heading);
-        font-size: 22px;
+        font-size: 1.375rem; /*22px */
         font-weight: var(--edux-font-light);
     }
 
     .heading-18-bold {
         font-family: var(--edux-font-heading);
-        font-size: 18px;
+        font-size: 1.125rem; /* 18px */
         font-weight: var(--edux-font-bold);
     }
 
     .heading-18-light {
         font-family: var(--edux-font-heading);
-        font-size: 18px;
+        font-size: 1.125rem; /* 18px */
         font-weight: var(--edux-font-light);
     }
 
     .heading-16-bold {
         font-family: var(--edux-font-heading);
-        font-size: 16px;
+        font-size: 1rem; /* 16px */
         font-weight: var(--edux-font-bold);
     }
 
     .heading-16-light {
         font-family: var(--edux-font-heading);
-        font-size: 16px;
+        font-size: 1rem; /* 16px */
         font-weight: var(--edux-font-light);
     }
 
     .heading-14-bold {
         font-family: var(--edux-font-heading);
-        font-size: 14px;
+        font-size: 0.875rem; /* 14px */
         font-weight: var(--edux-font-bold);
     }
 
     .heading-14-light {
         font-family: var(--edux-font-heading);
-        font-size: 14px;
+        font-size: 0.875rem; /* 14px */
         font-weight: var(--edux-font-light);
     }
 
     .heading-12-bold {
         font-family: var(--edux-font-heading);
-        font-size: 12px;
+        font-size: 0.75rem; /* 12px */
         font-weight: var(--edux-font-bold);
     }
 
     .heading-12-light {
         font-family: var(--edux-font-heading);
-        font-size: 12px;
+        font-size: 0.75rem; /* 12px */
         font-weight: var(--edux-font-light);
     }
 
     .heading-11-semi-bold {
         font-family: var(--edux-font-heading);
-        font-size: 11px;
+        font-size: 0.688rem; /* 11px */
         font-weight: var(--edux-font-semi-bold);
     }
 
     .body-18 {
         font-family: var(--edux-font-body);
-        font-size: 18px;
+        font-size: 1.125rem; /* 18px */
         font-weight: var(--edux-font-normal);
     }
 
@@ -163,25 +163,25 @@ export default css`
 
     .body-16 {
         font-family: var(--edux-font-body);
-        font-size: 16px;
+        font-size: 1rem; /* 16px */
         font-weight: var(--edux-font-normal);
     }
 
     .body-14 {
         font-family: var(--edux-font-body);
-        font-size: 14px;
+        font-size: 0.875rem; /* 14px */
         font-weight: var(--edux-font-normal);
     }
 
     .body-12 {
         font-family: var(--edux-font-body);
-        font-size: 12px;
+        font-size: 0.75rem; /* 12px */
         font-weight: var(--edux-font-normal);
     }
 
     .body-11 {
         font-family: var(--edux-font-body);
-        font-size: 11px;
+        font-size: 0.688rem; /* 11px */
         font-weight: var(--edux-font-normal);
     }
 
@@ -189,51 +189,51 @@ export default css`
 
     .number-240 {
         font-family: var(--edux-font-number-label);
-        font-size: 240px;
+        font-size: 15rem; /* 240px */
         font-weight: var(--edux-font-light);
     }
 
     .number-120 {
         font-family: var(--edux-font-number-label);
-        font-size: 120px;
+        font-size: 7.5rem; /* 120px */
         font-weight: var(--edux-font-light);
     }
 
     .number-48 {
         font-family: var(--edux-font-number-label);
-        font-size: 48px;
+        font-size: 3rem; /* 48px */
         font-weight: var(--edux-font-light);
     }
     
     .number-36 {
         font-family: var(--edux-font-number-label);
-        font-size: 36px;
+        font-size: 2.25rem; /* 36px */
         font-weight: var(--edux-font-light);
     }
 
     .number-11 {
         font-family: var(--edux-font-number-label);
-        font-size: 11px;
+        font-size: 0.688rem; /* 11px */
         font-weight: 500;
     }
 
     .label-14 {
         font-family: var(--edux-font-number-label);
-        font-size: 14px;
+        font-size: 0.875rem; /* 14px */
         font-weight: var(--edux-font-light);
         text-transform: uppercase;
     }
 
     .label-12 {
         font-family: var(--edux-font-number-label);
-        font-size: 12px;
+        font-size: 0.75rem; /* 12px */
         font-weight: var(--edux-font-light);
         text-transform: uppercase;
     }
 
     .label-11 {
         font-family: var(--edux-font-number-label);
-        font-size: 11px;
+        font-size: 0.688rem; /* 11px */
         font-weight: var(--edux-font-light);
         text-transform: uppercase;
     }    
