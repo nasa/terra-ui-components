40d4071a9947642ffab829a043323685766f0b87
amber.j.lee <amber.j.lee@nasa.gov>
2024-05-28 16:43:31 -0400

    style and html

--- src/components/chip/chip.component.ts
+++ src/components/chip/chip.component.ts
@@ -28,11 +28,23 @@ export default class EduxChip extends EduxElement {
     @property() attr = 'example'
 
     @watch('example')
-    handleExampleChange() {
-        // do something
+    hide() {
+        var chip = document.getElementById('Chip1');
+        chip.style.display = "none"
     }
 
     render() {
-        return html` <slot></slot> `
+        return html` 
+        <slot>
+            <div id="Chip1" class="chip" >
+                <div class="chip-content">Chip Content</div>
+                <div class="chip-close" onclick="hide()">
+                    <svg class="chip-svg" focusable="false" viewBox="0 0 500 500" aria-hidden="true">
+                        <path d="M 227 56.036 H 265 V 227.037 H 436 V 265.036 H 265 V 436.036 H 227 V 265.036 H 56 V 227.037 H 227 Z" style="fill: #909090; paint-order: fill; fill-rule: nonzero; stroke-width: 0px; transform-origin: 246px 246.036px;" transform="matrix(0.707107007504, 0.707107007504, -0.707107007504, 0.707107007504, -9.98393e-7, -0.000026419743)"></path>
+                    </svg>
+                
+                </div>
+            </div>
+        </slot> `
     }
 }
--- src/components/chip/chip.styles.ts
+++ src/components/chip/chip.styles.ts
@@ -1,7 +1,57 @@
 import { css } from 'lit';
 
 export default css`
-    :host {
-        display: block;
+    .chip{
+        display: inline-flex;
+        flex-direction: row;
+        background-color: #ffffff;
+        border: blue;
+        cursor: default;
+        height: 30px;
+        outline: #0090f0;
+        outline-style: solid;
+        outline-width: 2px;
+        border-radius: 2px;
+        padding: 0;
+        margin: 5px;
+        font-size: 15px;
+        font-weight: bold;
+        color: #909090;
+        font-family:"Open Sans", sans-serif;
+        white-space: nowrap;
+        align-items: center;
+        vertical-align: middle;
+        text-decoration: none;
+        justify-content: center;
     }
+
+    .chip-content{
+        cursor: inherit;
+        display: flex;
+        align-items: center;
+        user-select: none;
+        white-space: nowrap;
+        padding-left: 12px;
+        padding-right: 12px;
+    }
+    .chip-svg{
+        color: #888888;
+        cursor: pointer;
+        height: auto;
+        margin: 4px 4px 0 -8px;
+        fill: currentColor;
+        width: 1em;
+        height: 1em;
+        display: inline-block;
+        font-size: 24px;
+        transition: fill 200ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
+        user-select: none;
+        flex-shrink: 0;
+    }
+
+    .chip-svg:hover {
+        visibility: "visible";
+        filter: invert(100%);
+    }
+
 `;
