c5a59692716a6b69b845be37158ad580c2fa1f5b
aritrivi <anthony.j.ritrivi@nasa.gov>
2024-06-04 16:23:51 -0400

    Initial theming of the map component.

--- src/components/map/map.component.ts
+++ src/components/map/map.component.ts
@@ -122,7 +122,7 @@ export default class EduxMap extends EduxElement {
         return html`
             <div>
                 <select
-                    class="map__select form-control"
+                    class="map__select form-control input"
                     @change=${this.map.handleShapeSelect}
                 >
                     <option>Select a Shape...</option>
--- src/components/map/map.styles.ts
+++ src/components/map/map.styles.ts
@@ -26,17 +26,10 @@ export default css`
 
     :host .form-control {
         display: block;
-        border-radius: 0;
         width: 100%;
         height: 36px;
         padding: 6px 12px;
-        font-size: 14px;
-        line-height: 1.42857143;
-        color: #555555;
-        background-color: #fff;
         background-image: none;
-        border: 1px solid #ccc;
-        border-radius: 4px;
         -webkit-box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.075);
         box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.075);
         -webkit-transition:
@@ -51,7 +44,6 @@ export default css`
         width: 100%;
         /* height: 1.7rem; */
         box-shadow: none;
-        border-radius: 0 !important;
         margin-bottom: 1rem;
     }
 `
