f7ccf178118a22699a5cd04f42d3026a5323a37a
aritrivi <anthony.j.ritrivi@nasa.gov>
2024-06-12 16:30:00 -0400

    Added CSV download of plot data to the plot.

--- src/components/plot/plot.component.ts
+++ src/components/plot/plot.component.ts
@@ -5,8 +5,15 @@ import componentStyles from '../../styles/component.styles.js'
 import EduxElement from '../../internal/edux-element.js'
 import styles from './plot.styles.js'
 import type { CSSResultGroup } from 'lit'
-import { newPlot, type Data, type Layout, type Config } from 'plotly.js-dist-min'
+import { newPlot, Icons, type Data, type Layout, type Config } from 'plotly.js-dist-min'
 
+interface plot {
+    type: string
+    mode: string
+    name: string
+    x: Array<number>
+    y: Array<number>
+}
 /**
  * @summary A web component for interactive graphs using Plotly.js.
  * @documentation https://disc.gsfc.nasa.gov/components/plot
@@ -28,7 +35,18 @@ export default class EduxPlot extends EduxElement {
     layout?: Partial<Layout> = {}
 
     @property()
-    config?: Partial<Config>
+    config?: Partial<Config> = {
+        modeBarButtonsToAdd: [
+            {
+                name: 'Download Data',
+                title: 'Download plot data as a CSV file',
+                icon: Icons.disk,
+                click: () => {
+                    this.downloadCSV()
+                }
+            }            
+        ]
+    }
 
     @property({ type: Array })
     data: Array<Partial<Data>> = []
@@ -62,7 +80,39 @@ export default class EduxPlot extends EduxElement {
         )
     }
 
+    downloadCSV() {
+        // TODO: properties x and y do not exist on data since it is of type Array<Partial<Data>> so they are flagged.
+        // I created a plot interface to define the x and y properties but data must be of type Array<Partial<Data>> to work with Plotly.
+        // Code still works though.
+        //const csvData = this.data.map((trace: plot) => {
+        const csvData = this.data.map(trace => {
+            return trace.x.map((x: any, i: number) => {
+                return {
+                    x: x,
+                    y: trace.y[i]
+                };
+            });
+        }).flat();
+
+        const csv = this.convertToCSV(csvData);
+        const blob = new Blob([csv], { type: 'text/csv' });
+        const url = window.URL.createObjectURL(blob);
+        const a = document.createElement('a');
+        a.setAttribute('href', url);
+        a.setAttribute('download', 'chart_data.csv');
+        a.style.display = 'none';
+        document.body.appendChild(a);
+        a.click();
+        document.body.removeChild(a);
+    }
+
+    convertToCSV(data: any[]): string {
+    const header = Object.keys(data[0]).join(",") + "\n";
+    const rows = data.map(obj => Object.values(obj).join(",")).join("\n");
+    return header + rows;
+}
     render() {
-        return html` <div part="base"></div> `
+        return html` <div part="base"></div>
+        `
     }
 }
