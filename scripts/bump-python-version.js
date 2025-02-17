cb10a4af9cc7204d1971e13e9f351cfd1332eed8
Jon Carlson <jdcarlso@gs6102dscmcjdca.ndc.nasa.gov>
2025-02-17 15:23:13 -0500

    feature: bump Python version at same time as NPM version

--- scripts/bump-python-version.js
+++ scripts/bump-python-version.js
@@ -1,32 +1,26 @@
+/**
+ * Sets the Python package version to the same version as in "package.json"
+ */
 import { execSync } from 'child_process'
+import fs from 'fs'
 
-// Get the latest tag from npm version commit
-const latestTag = execSync('git describe --tags --abbrev=0').toString().trim()
-const previousTag = execSync('git tag --sort=-v:refname | sed -n 2p')
-    .toString()
-    .trim()
+try {
+    // Get the latest tag from Git (will return something like: v1.0.0)
+    const latestTag = execSync('git describe --tags --abbrev=0').toString().trim()
+    const newVersion = latestTag.replace('v', '')
 
-if (!latestTag || !previousTag) {
-    console.log('Could not determine previous version.')
+    // Write the new version to the VERSION file
+    fs.writeFileSync('VERSION', newVersion + '\n', 'utf8')
+
+    // Stage the VERSION file
+    // since "npm version" automatically commits, we'll need to amend that commit with this VERSION change
+    execSync('git add VERSION')
+
+    // Amend the previous commit to include the VERSION file
+    execSync('git commit --amend --no-edit')
+
+    console.log('Updated Python version to:', newVersion)
+} catch (error) {
+    console.error('Error updating Python version:', error.message)
     process.exit(1)
 }
-
-const [latestMajor, latestMinor, latestPatch] = latestTag
-    .replace(/^v/, '')
-    .split('.')
-    .map(Number)
-const [prevMajor, prevMinor, prevPatch] = previousTag
-    .replace(/^v/, '')
-    .split('.')
-    .map(Number)
-
-let versionType = 'unknown'
-if (latestMajor > prevMajor) {
-    versionType = 'major'
-} else if (latestMinor > prevMinor) {
-    versionType = 'minor'
-} else if (latestPatch > prevPatch) {
-    versionType = 'patch'
-}
-
-console.log(versionType)
