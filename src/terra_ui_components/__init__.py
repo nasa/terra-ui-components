0e7acbf4022e2f3987316fa77c8773b57359f154
Ben Forshey <benjamin.a.forshey@nasa.gov>
2025-03-28 16:02:28 -0400

    feat(login): create MVP EDL login component for Jupyter Notebook integrations

--- src/terra_ui_components/__init__.py
+++ src/terra_ui_components/__init__.py
@@ -2,4 +2,5 @@ from .base import TerraBaseWidget
 from .date_range_slider import TerraDateRangeSlider
 from .giovanni_search import TerraGiovanniSearch
 from .time_series import TerraTimeSeries
-__all__ = ["TerraBaseWidget", "TerraDateRangeSlider", "TerraGiovanniSearch", "TerraTimeSeries"]
+from .login import TerraLogin
+__all__ = ["TerraBaseWidget", "TerraDateRangeSlider", "TerraGiovanniSearch", "TerraTimeSeries", "TerraLogin"]
