8b1d4ecf639ec0269bb69050ba9b76029fc427fa
Jon Carlson <jdcarlso@gs6102dscmcjdca.ndc.nasa.gov>
2025-06-13 12:21:35 -0400

    bugfix: remove GiovanniSearch from python package

--- src/terra_ui_components/__init__.py
+++ src/terra_ui_components/__init__.py
@@ -1,9 +1,8 @@
 from .base import TerraBaseWidget
 from .date_range_slider import TerraDateRangeSlider
-from .giovanni_search import TerraGiovanniSearch
 from .time_series import TerraTimeSeries
 from .login import TerraLogin
 from .dialog import TerraDialog
 from .date_picker import TerraDatePicker
 from .data_rods import TerraDataRods
-__all__ = ["TerraBaseWidget", "TerraDateRangeSlider", "TerraGiovanniSearch", "TerraTimeSeries", "TerraLogin", "TerraDialog", "TerraDatePicker", "TerraDataRods"]
+__all__ = ["TerraBaseWidget", "TerraDateRangeSlider", "TerraTimeSeries", "TerraLogin", "TerraDialog", "TerraDatePicker", "TerraDataRods"]
