eac24d69d5b51243c735456ec81464dcb500397a
Jon Carlson <jdcarlson@gmail.com>
2025-07-29 15:43:25 -0400

    chore: cleanup

--- src/components/data-subsetter-history/data-subsetter-history.ts
+++ src/components/data-subsetter-history/data-subsetter-history.ts
@@ -1,12 +1,12 @@
-import TerraDataSubsetterHistory from './data-subsetter-history.component.js';
+import TerraDataSubsetterHistory from './data-subsetter-history.component.js'
 
-export * from './data-subsetter-history.component.js';
-export default TerraDataSubsetterHistory;
+export * from './data-subsetter-history.component.js'
+export default TerraDataSubsetterHistory
 
-TerraDataSubsetterHistory.define('terra-data-subsetter-history');
+TerraDataSubsetterHistory.define('terra-data-subsetter-history')
 
 declare global {
     interface HTMLElementTagNameMap {
-      'terra-data-subsetter-history': TerraDataSubsetterHistory;
+        'terra-data-subsetter-history': TerraDataSubsetterHistory
     }
 }
