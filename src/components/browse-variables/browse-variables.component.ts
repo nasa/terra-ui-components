9cf2e0c753cf849bb1aed98fd0cbc2d13b84e877
aritrivi <anthony.j.ritrivi@nasa.gov>
2025-08-29 16:46:38 -0400

    Theming the variable browse to match the rest of the terra components and to match the Figma mockup.

--- src/components/browse-variables/browse-variables.component.ts
+++ src/components/browse-variables/browse-variables.component.ts
@@ -439,7 +439,7 @@ export default class TerraBrowseVariables extends TerraElement {
                                             />
                                             <strong
                                                 >${variable.dataFieldLongName}</strong
-                                            ><br />
+                                            >
                                             <span
                                                 >${variable.dataProductShortName}
                                                 &bull;
@@ -455,21 +455,19 @@ export default class TerraBrowseVariables extends TerraElement {
                                                 ${variable.dataFieldLongName}
                                             </h4>
                                             <p>
-                                                <strong>Spatial Resolution</strong
-                                                ><br />
+                                                <strong>Spatial Resolution</strong>
                                                 ${variable.dataProductSpatialResolution}
                                             </p>
                                             <p>
-                                                <strong>Temporal Coverage</strong
-                                                ><br />
+                                                <strong>Temporal Coverage</strong>
                                                 ${variable.dataProductBeginDateTime}&puncsp;&ndash;&puncsp;${variable.dataProductEndDateTime}
                                             </p>
                                             <p>
-                                                <strong>Region Coverage</strong><br />
+                                                <strong>Region Coverage</strong>
                                                 ${variable.dataProductWest},${variable.dataProductSouth},${variable.dataProductEast},${variable.dataProductNorth}
                                             </p>
                                             <p>
-                                                <strong>Dataset</strong><br />
+                                                <strong>Dataset</strong>
                                                 ${variable.dataProductShortName}_${variable.dataProductVersion}
                                             </p>
                                         </sl-drawer>
