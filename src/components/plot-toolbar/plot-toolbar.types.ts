2078c764581d9fcf34b644200843a4c9c67853bc
Andy Babay <andy.r.babay@nasa.gov>
2025-09-11 14:12:22 -0400

    Styling for geotiff panel

--- src/components/plot-toolbar/plot-toolbar.types.ts
+++ src/components/plot-toolbar/plot-toolbar.types.ts
@@ -1,3 +1,3 @@
-export type MenuNames = 'download' | 'help' | 'information' | 'jupyter' | null
+export type MenuNames = 'download' | 'help' | 'information' | 'jupyter' | 'GeoTIFF' | null
 
 export type DataType = 'csv' | 'geotiff' | null
