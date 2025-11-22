6b352fc2c03589fe7fc159d0f780a08c9b8c5ac5
Jon Carlson <jdcarlson@gmail.com>
2025-11-22 10:16:14 -0500

    0.0.115

--- src/components/plot-toolbar/notebooks/time-averaged-map-notebook.ts
+++ src/components/plot-toolbar/notebooks/time-averaged-map-notebook.ts
@@ -21,7 +21,7 @@ export function getTimeAveragedMapNotebook(host: TerraPlotToolbar) {
         {
             id: '2733501b-0de4-4067-8aff-864e1b4c76cb',
             cell_type: 'code',
-            source: '%pip install -q "terra_ui_components==0.0.114" "anywidget==0.9.15" "pandas"',
+            source: '%pip install -q "terra_ui_components==0.0.115" "anywidget==0.9.15" "pandas"',
             metadata: {
                 trusted: true,
             },
